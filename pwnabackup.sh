347457bbb01269df2047a175267523e84b5a63c7
gh0stshell <gh0stshell@users.noreply.github.com>
2019-10-20 22:31:19 -0700

    Initial Commit
    Version 1.3b

---
+++ pwnabackup.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#
+# Version 1.3b
+#
+# Modded version of original backup
+# script that does not requite root
+# to be setup/enabled and puts the
+# output zip file in the pi home
+# under /home/pi/pwnabackup
+#
+# Usage: sudo ./pwnabackup.sh nameOFzipFile.zip
+#
+
+# name of the ethernet gadget interface on the host
+UNIT_HOSTNAME=$(cat /etc/hostname)
+
+# output backup zip file
+OUTPUT=$(pwnagotchi-backup.zip)
+
+# temporary folder
+BACKUP_LOCATION=/home/pi/pwnabackup
+
+# what to backup
+FILES_TO_BACKUP=(
+  /root/brain.nn
+  /root/brain.json
+  /root/handshakes
+  /etc/pwnagotchi/
+  /etc/hostname
+  /etc/hosts
+  /etc/motd
+  /var/log/pwnagotchi.log
+)
+
+echo "@ backing up $UNIT_HOSTNAME to $OUTPUT ..."
+
+# Deleting old backups before creating new backup archive
+rm -rf "$BACKUP_LOCATION"
+
+# Create folders & Copy files to backup location
+for file in "${FILES_TO_BACKUP[@]}"; do
+  dir=$(dirname $file)
+  echo "  $file -> $BACKUP_LOCATION$dir/"
+  mkdir -p "$BACKUP_LOCATION/$dir"
+  sudo cp -R /$file "$BACKUP_LOCATION$dir/"
+done
+
+# Archive copied files
+ZIPFILE="$PWD/$OUTPUT"
+pushd $PWD
+cd "$BACKUP_LOCATION"
+zip -r -9 -q "$ZIPFILE" .
+popd
+
+#Removing copied files but leaving ZIPFILE
+rm -rf "$BACKUP_LOCATION/root"
+rm -rf "$BACKUP_LOCATION/etc"
+rm -rf "$BACKUP_LOCATION/var"
